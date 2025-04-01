947474e706064d7abdc8056f0bf274743da5d873
COT-WORLD <hardikchaudhary.aus@gmail.com>
2025-04-01 19:17:19 +1100

    Displayingm critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Game } from "../hooks/useGames";
-import { Card, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import GamePlatformIconList from "./GamePlatformIconList";
+import GameCriticScore from "./GameCriticScore";
 
 interface Props {
   game: Game;
@@ -14,9 +15,12 @@ const GameCard = ({ game }: Props) => {
       </Card.Header>
       <Card.Body>
         <Heading>{game.name}</Heading>
-        <GamePlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between">
+          <GamePlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <GameCriticScore score={game.metacritic} />
+        </HStack>
       </Card.Body>
     </Card.Root>
   );
---
+++ src/components/GameCriticScore.tsx
@@ -0,0 +1,16 @@
+import { Badge } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  score: number;
+}
+const GameCriticScore = ({ score }: Props) => {
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge colorPalette={color} fontSize="14px" paddingX={2} borderRadius="4px">
+      {score}
+    </Badge>
+  );
+};
+
+export default GameCriticScore;
